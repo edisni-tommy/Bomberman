2d7cfea2e3c54d141a11817dc596174c9b3b2f1c
edisny-tommy <gianglht2132003@gmail.com>
2022-10-06 23:25:58 +0700

    update image

--- src/main/java/UI/Menu/Image.java
+++ src/main/java/UI/Menu/Image.java
@@ -2,41 +2,55 @@ package UI.Menu;
 
 import Cores.Main;
 import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
 import com.jme3.ui.Picture;
 
 public class Image {
     protected final Picture pic;
-    private final Vector2f pos = new Vector2f(0, 0);
-    private final Vector2f size;
+    private final Vector2f pos2D = new Vector2f(0, 0);
+    private final Vector2f size = new Vector2f();
 
     public Image(Vector2f pos, Vector2f size, String path) {
-        this.size = size;
         pic = new Picture(path);
         setPos(pos);
-        pic.setWidth(size.x);
-        pic.setHeight(size.y);
+        setSize(size);
         pic.setImage(Main.ASSET_MANAGER, path, true);
     }
 
-    public Image(Vector2f size, String path) {
-        this.size = size;
+    public Image(Vector3f position, Vector2f size, String path) {
         pic = new Picture(path);
-        pic.setHeight(size.y);
-        pic.setWidth(size.x);
+        pic.setImage(Main.ASSET_MANAGER, path, true);
+        setPos(position);
+        setSize(size);
+    }
+    public Image(Vector2f size, String path) {
+        pic = new Picture(path);
+        setSize(size);
         pic.setImage(Main.ASSET_MANAGER, path, true);
     }
 
     public void setPos(Vector2f pos) {
-        this.pos.x = pos.x;
-        this.pos.y = pos.y;
+        this.pos2D.x = pos.x;
+        this.pos2D.y = pos.y;
         pic.setPosition(pos.x, pos.y);
     }
+
+    public void setPos(Vector3f pos) {
+        pic.setLocalTranslation(pos);
+    }
+
+    public void setSize(Vector2f size) {
+        this.size.x = size.x;
+        this.size.y = size.y;
+        pic.setWidth(size.x);
+        pic.setHeight(size.y);
+    }
     public float getPosX() {
-        return pos.x;
+        return pos2D.x;
     }
 
     public float getPosY() {
-        return pos.y;
+        return pos2D.y;
     }
 
     public float getWidth() {
--- src/main/java/UI/PlayerStatus/BombStatusBar.java
+++ src/main/java/UI/PlayerStatus/BombStatusBar.java
@@ -1,7 +1,7 @@
 package UI.PlayerStatus;
 
 import Cores.Main;
-import UI.GUI.Image;
+import UI.Menu.Image;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
@@ -27,16 +27,16 @@ public class BombStatusBar extends PlayerStatus {
         float x = position.x - 30;
         float y = position.y;
         float z = position.z;
-        background.setPosition(new Vector3f(x, y, z));
-        border.setPosition(new Vector3f(x, y, z));
+        background.setPos(new Vector3f(x, y, z));
+        border.setPos(new Vector3f(x, y, z));
         for (int i = 0; i < currentBomb; ++ i) {
             Image filled = new Image(new Vector2f(60f/maxBomb, 15f), "Textures/Bar/Status/fill.png");
-            filled.setPosition(new Vector3f(x + (60f/maxBomb) * i, y, z));
+            filled.setPos(new Vector3f(x + (60f/maxBomb) * i, y, z));
             fill.add(filled);
         }
         for (int i = 0; i < maxBomb; ++ i) {
             Image p = new Image(new Vector2f(60f/maxBomb, 15f), "Textures/Bar/Status/part.png");
-            p.setPosition(new Vector3f(x + (60f/maxBomb) * i, y, z));
+            p.setPos(new Vector3f(x + (60f/maxBomb) * i, y, z));
             fill.add(p);
         }
         show();
@@ -64,34 +64,34 @@ public class BombStatusBar extends PlayerStatus {
         float x = position.x - 30;
         float y = position.y;
         float z = position.z;
-        background.setPosition(new Vector3f(x, y, z));
-        border.setPosition(new Vector3f(x, y, z));
+        background.setPos(new Vector3f(x, y, z));
+        border.setPos(new Vector3f(x, y, z));
         for (int i = 0; i < currentBomb; ++ i) {
             Image filled = new Image(new Vector2f(60f/maxBomb, 15f), "Textures/Bar/Status/fill.png");
-            filled.setPosition(new Vector3f(x + (60f/maxBomb) * i, y, z));
+            filled.setPos(new Vector3f(x + (60f/maxBomb) * i, y, z));
             fill.add(filled);
         }
         if (currentBomb < maxBomb) {
             Image filled = new Image(new Vector2f((60f * (4.0f - currentCooldown))/(maxBomb * 4.0f), 15f), "Textures/Bar/Status/cool_down.png");
-            filled.setPosition(new Vector3f(x + (60f/maxBomb) * currentBomb, y, z));
+            filled.setPos(new Vector3f(x + (60f/maxBomb) * currentBomb, y, z));
             fill.add(filled);
         }
         for (int i = 0; i < maxBomb; ++ i) {
             Image p = new Image(new Vector2f(60f/maxBomb, 15f), "Textures/Bar/Status/part.png");
-            p.setPosition(new Vector3f(x + (60f/maxBomb) * i, y, z));
+            p.setPos(new Vector3f(x + (60f/maxBomb) * i, y, z));
             part.add(p);
         }
         show();
     }
 
     private void show() {
-        border.show();
-        background.show();
+        border.display();
+        background.display();
         for (Image image: fill) {
-            image.show();
+            image.display();
         }
         for (Image image: part) {
-            image.show();
+            image.display();
         }
     }
 }
